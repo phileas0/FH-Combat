b23ec89c1505fecacd9cdde7067cd20fddff160a
LaibaWarraich <146174828+LaibaWarraich@users.noreply.github.com>
2024-12-30 16:04:50 +0100

    start menu added
    basic start menu with play, settings and quit button

---
+++ FH-Combat/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+    
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
